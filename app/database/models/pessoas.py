2f08e6d88e3b22f5b6fd02b9ea15fe66133ea347
Pedro Henrique <pedrohbf12332@gmail.com>
2026-02-27 10:30:37 -0300

    feat: table pessoas

---
+++ app/database/models/pessoas.py
@@ -0,0 +1,14 @@
+from app.database.db import Base
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from datetime import datetime, timezone
+
+class pessoas(Base): 
+    __tablename__ = 'pessoas'
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String)
+    email = Column(String, unique=True)
+    telefone = Column(Integer, unique=True, )
+    createdAt = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updatedAt = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
