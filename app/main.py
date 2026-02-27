020a23a62eb6e2cb39bbc650a5a86d6277ccb9c6
Pedro Henrique <pedrohbf12332@gmail.com>
2026-02-27 08:47:43 -0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from datetime import datetime
+from pydantic import BaseModel
+
+PORT = 3000
+app = FastAPI()
+
+class HealthResponse(BaseModel):
+    status: str
+    date: datetime
+
+@app.get("/", response_model=HealthResponse)
+def hello():
+    return HealthResponse(
+        date=datetime.now(),
+        status= "Alive"
+    )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=PORT,
+        reload=True
+    )
