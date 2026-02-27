33f21e77cb8735d8b933a659cb532c0bbac0ea26
Pedro Henrique <pedrohbf12332@gmail.com>
2026-02-27 14:50:07 -0300

    change: titles

--- app/modules/pessoas/pessoas_controller.py
+++ app/modules/pessoas/pessoas_controller.py
@@ -14,22 +14,22 @@ def get_service(db: Session = Depends(get_db)):
     return service
 
 
-@router.get("/", response_model=list[getPessoa])
+@router.get("/", response_model=list[getPessoa], summary="Listar todas pessoas")
 async def listar(service: PessoasService = Depends(get_service)):
     return service.listar_pessoas()
 
-@router.get("/{id}", response_model=getPessoa)
+@router.get("/{id}", response_model=getPessoa, summary="Listar pessoa por id")
 async def listarPorId(id: int, service: PessoasService = Depends(get_service)):
     return service.listar_pessoas_id(id)
 
-@router.post("/", response_model=getPessoa)
+@router.post("/", response_model=getPessoa, summary="Criar pessoa")
 async def criar(service: PessoasService = Depends(get_service), pessoa: getPessoa = Body(getPessoa)):
     return service.criar(pessoa)
 
-@router.put("/{id}", response_model=getPessoa)
+@router.put("/{id}", response_model=getPessoa, summary="Atualizar pessoa por id")
 async def atualizar(id: int, service: PessoasService = Depends(get_service), pessoa: getPessoa = Body(getPessoa)):
     return service.atualizar_pessoa(id, pessoa)
 
-@router.delete("/{id}", response_model=Dict[str, str])
+@router.delete("/{id}", response_model=Dict[str, str], summary="Deletar pessoa por id")
 async def apagar(id: int, service: PessoasService = Depends(get_service)):
     return service.apagar_pessoa(id)
